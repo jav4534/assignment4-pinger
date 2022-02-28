55b1b4c6fd0b051b9ae62c2368dc72a1dd512d14
Jose Velazquez <jav4534@nyu.edu>
2022-02-27 23:01:21 -0500

    Updated solution.py with the vars returning min, avg, max and stddev as required

--- solution.py
+++ solution.py
@@ -1,4 +1,14 @@
 #Author: jav4534
+#    0                   1                   2                   3
+#    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+#   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#   |     Type(8)   |     Code(0)   |          Checksum             |
+#   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#   |           Identifier          |        Sequence Number        |
+#   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#   |                             Payload                           |
+#   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
 from socket import *
 import os
 import sys
@@ -6,6 +16,7 @@ import struct
 import time
 import select
 import binascii
+import statistics
 # Should use stdev
 
 ICMP_ECHO_REQUEST = 8
@@ -55,10 +66,36 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         recPacket, addr = mySocket.recvfrom(1024)
 
         # Fill in start
-        
+        #ipHeader = recPacket[:20]
+        #print(ipHeader)
+        #ipHeaderVersion, ipHeaderTypeOfSvc, ipHeaderLength, ipHeaderIdID, \
+        #ipHeaderFlags, ipHeaderTTL, ipHeaderProtocol, ipHeaderChecksum, \
+        #ipHeaderSrcIP, ipHeaderDestIP = struct.unpack(
+        #    "!BBHHHBBHII", ipHeader
+        #)
+
+        icmpHeader = recPacket[20:28]
+        type, code, checksum, processID, sequence = struct.unpack("bbHHh", icmpHeader)
+
+        #print("type:")
+        #print(type)
+        #print("code:")
+        #print(code)
+        #print("checksum:")
+        #print(checksum)
+        #print("processID:")
+        #print(processID)
+        #print("sequence #:")
+        #print(sequence)
+
+        if processID == ID:
+            bytesInDouble = struct.calcsize("d")
+            timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
+            return timeReceived - timeSent
+
         # Fetch the ICMP header from the IP packet
         #print(ID)
-        #print("received Packet:")        
+        #print("received Packet:")
         #print(recPacket)
         #print("addr:")
         #print(addr)
@@ -70,14 +107,14 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
             return "Request timed out."
 
 
-def sendOnePing(mySocket, destAddr, ID):
+def sendOnePing(mySocket, destAddr, id):
     #print("***** ***** sendOnePing ***** *****")
     # Header is type (8), code (8), checksum (16), id (16), sequence (16)
 
-    myChecksum = 0
+    #myChecksum = 0
     # Make a dummy header with a 0 checksum
     # struct -- Interpret strings as packed binary data
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, 0, id, 1)
     #print("header: ")
     #print(header)
     data = struct.pack("d", time.time())
@@ -96,16 +133,16 @@ def sendOnePing(mySocket, destAddr, ID):
         myChecksum = htons(myChecksum)
 
 
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, id, 1)
     #print("header: ")
     #print(header)
     #print("data: ")
     #print(data)
     packet = header + data
-    
+
     #print("packet: ")
     #print(packet)
-    
+
     mySocket.sendto(packet, (destAddr, 1))  # AF_INET address must be tuple, not str
 
 
@@ -120,9 +157,9 @@ def doOnePing(destAddr, timeout):
     # SOCK_RAW is a powerful socket type. For more details:   http://sockraw.org/papers/sock_raw
     mySocket = socket(AF_INET, SOCK_RAW, icmp)
 
-    myID = os.getpid() & 0xFFFF  # Return the current process i
-    sendOnePing(mySocket, destAddr, myID)
-    delay = receiveOnePing(mySocket, myID, timeout, destAddr)
+    processID = os.getpid() & 0xFFFF  # Return the current process i
+    sendOnePing(mySocket, destAddr, processID)
+    delay = receiveOnePing(mySocket, processID, timeout, destAddr)
     mySocket.close()
     return delay
 
@@ -133,14 +170,19 @@ def ping(host, timeout=1):
     print("Pinging " + dest + " using Python:")
     print("")
     vars = []
+    temp = []
     # Calculate vars values and return them
     #  vars = [str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)),str(round(stdev(stdev_var), 2))]
     # Send ping requests to a server separated by approximately one second
     for i in range(0,4):
         delay = doOnePing(dest, timeout)
+        #print("delay:")
         print(delay)
-        vars.append(delay)
+        temp.append(delay)
         time.sleep(1)  # one second
+
+    vars = [str(round(min(temp), 2)), str(round((sum(temp) / len(temp)), 2)), str(round(max(temp), 2)),str(round(statistics.stdev(temp), 2))]
+    print(vars)
 
     return vars
 
